1ab9b07fdf03bdd8051edb4eacc5ce5c6d0c6167
Sungguk Lim <limasdf@gmail.com>
2019-12-12 13:08:27 +0900

    Make searchbox template works
    InstantSearch's SearchBox widget used to be loaed at
    <body onload="buildWidget()">. It was working but customizing
    UI using Mustache didn't work.
    
    This change move the widget loading code to separate
    file(assets/js/search_widget.js). and load it at the end of
    default.html. Now template using Mustache is working.

--- assets/js/search_widget.js
+++ assets/js/search_widget.js
@@ -1,1 +1,31 @@
-dummy
+const search = instantsearch({
+  indexName: 'jekyll_blog',
+  searchClient: algoliasearch(
+    'I2X1XFAM0D',
+    '9db8946e9f3cae61db59553cf54dee6b'
+  ),
+	searchFunction: function(helper) {
+    const container = document.querySelector('#hits');
+
+		if (helper.state.query === '') {
+			container.style.display = 'none';
+		} else {
+			container.style.display = '';
+			helper.search();
+		}
+	}
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+  }),
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      empty: 'No results for <q>{{ query }}</q>',
+    },
+  })
+]);
+
+search.start();
